ee3860e81c3815f9284dca9b2b6d2e26d59b5c69
Manuel Parra <manuel.parra1@gmail.com>
2022-12-12 14:52:24 -0600

    exercises modules

--- prepare.py
+++ prepare.py
@@ -1,3 +1,29 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import env
+
+def prep_iris(df):
+    to_drop = ['species_id', 'measurement_id']
+    
+    df.drop(columns=to_drop, inplace=True)
+    df.rename(columns={'species_name': 'species'}, inplace=True)
+    dummy_iris = pd.get_dummies(df.species, drop_first=True)
+    df = pd.concat([df, dummy_iris], axis=1)
+    return df
+
+def prep_titanic(df):
+    df.drop(columns=['Unnamed: 0','passenger_id','class','deck','embarked'], inplace=True)
+    dummy_titanic = pd.get_dummies(df[['sex','embark_town']], drop_first=True)
+    df = pd.concat([df, dummy_titanic], axis=1)
+    return df
+
+def prep_telco(df):
+    df = df.drop(columns=['internet_service_type_id', 'contract_type_id', 'payment_type_id'])
+    dummy_df = pd.get_dummies(df[['multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies']], drop_first=True)
+    df = pd.concat( [df, dummy_df], axis=1 )
+    return df
+
 def split_dataset(df):
     import pandas as pd
     import numpy as np
